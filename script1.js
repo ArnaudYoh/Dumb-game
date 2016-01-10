87688bdcba91d4096de84747ac90534cbfebe214
naunauyoh <arnaudyoh@gmail.com>
2016-01-10 19:14:56 +0000

    start js bis

--- script1.js
+++ script1.js
@@ -1,8 +1,13 @@
 var main = function(){
-	
+	$('.one').click(function(){
+		$('.players').fadeOut(); 
+
+
+	}); 
+	$('.two').click(function(){
+		$('.players').fadeOut();
+		
+	}); 
 }
-
-
-
 
 $(document).ready(main); 
