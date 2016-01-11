aa8a021982dc12593fc11074c057ea65c16e9412
naunauyoh <arnaudyoh@gmail.com>
2016-01-11 19:16:34 +0000

    some advancement

--- script1.js
+++ script1.js
@@ -1,9 +1,16 @@
+var sologame = function(){
+	
+}
+
+
+
 var one = function(){
 	$('.players').fadeOut(300); 
 	setTimeout(function() {
 		$('.title').append("<div class='secondtlt'> Solo Mode</div>"); 
 		$('.start').fadeIn(200); 
 	}, 500); 
+	$('.start').click(sologame); 
 }
 
 var two = function(){
@@ -12,6 +19,7 @@ var two = function(){
 		$('.title').append("<div class='secondtlt'> Versus Mode</div>"); 
 		$('.start').fadeIn(200); 
 	}, 500); 
+	$('.start').click(versusgame);
 }
 
 
