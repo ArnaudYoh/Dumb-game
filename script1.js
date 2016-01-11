700c7ba565c9193c3be59dfdb14dc3fce0aaebff
naunauyoh <arnaudyoh@gmail.com>
2016-01-11 14:19:53 +0000

    startbtn

--- script1.js
+++ script1.js
@@ -1,13 +1,24 @@
+var one = function(){
+	$('.players').fadeOut(300); 
+	setTimeout(function() {
+		$('.title').append("<div class='secondtlt'> Solo Mode</div>"); 
+		$('.start').fadeIn(200); 
+	}, 500); 
+}
+
+var two = function(){
+	$('.players').fadeOut(300);
+	setTimeout(function() {
+		$('.title').append("<div class='secondtlt'> Versus Mode</div>"); 
+		$('.start').fadeIn(200); 
+	}, 500); 
+}
+
+
 var main = function(){
-	$('.one').click(function(){
-		$('.players').fadeOut(); 
-
-
-	}); 
-	$('.two').click(function(){
-		$('.players').fadeOut();
-		
-	}); 
+	$('.start').fadeOut(0);
+	$('.one').click(one); 
+	$('.two').click(two); 
 }
 
 $(document).ready(main); 
