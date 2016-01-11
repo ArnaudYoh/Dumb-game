68ce8c5bcff29266a97a207db466461c21c119a6
naunauyoh <arnaudyoh@gmail.com>
2016-01-11 22:33:20 +0000

    advanced in js

--- script1.js
+++ script1.js
@@ -1,48 +1,56 @@
-var countdown = function(number){
-	if (number === 0) {
-		$('.secondtlt').append('<br class="temp"><br class="temp"><div class="temp">Start !</div>'); 
-		$('.temp').delay(100).fadeOut(300); 
+//Displays countdown + start
+var countdown = function(count){
+	if (count === 0) {
 		return; 
 	}
 	setTimeout(function(){
-		countdown(number-1);  
+		countdown(count-1);  
 	}, 600); 
-	$('.secondtlt').append('<br class="temp"><br class="temp"><div class="temp">'+number+'</div>'); 
+	$('.secondtlt').append('<br class="temp"><br class="temp"><div class="temp">'+count+'</div>'); 
 	$('.temp').delay(300).fadeOut(200); 
 } 
 
-var game = function(number){
+//manages calls to countdown when starting
+var game = function(){
 	$('.start').fadeOut(0); 
-	countdown(3); 
+	countdown(3);
+	$('.secondtlt').append('<br class="temp"><br class="temp"><div class="temp">Start !</div>'); 
+	$('.temp').delay(100).fadeOut(300); 
 }
 
+//Set up of start button + title solo mode
 var one = function(){
 	$('.players').fadeOut(300); 
 	setTimeout(function() {
 		$('.title').append("<div class='secondtlt'> Solo Mode</div>"); 
 		$('.start').fadeIn(200); 
 	}, 500); 
-	$('.start').click(function(){
-		game(1)
-	}); 
 }
 
+//Set up of start button + title versus mode
 var two = function(){
 	$('.players').fadeOut(300);
 	setTimeout(function() {
 		$('.title').append("<div class='secondtlt'> Versus Mode</div>"); 
 		$('.start').fadeIn(200); 
 	}, 500); 
-	$('.start').click(function(){
-		game(2)
-	});
 }
 
-
+//beginnig function
 var main = function(){
+	var choice = 0; 
 	$('.start').fadeOut(0);
-	$('.one').click(one); 
-	$('.two').click(two); 
+	$('.one').click(function(){
+		one(); 
+		choice = 1; 
+	}); 
+	$('.two').click(function(){
+		two();
+		choice = 2; 
+	});
+	$('.start').click(function(){
+		game(); 
+	}); 
 }
 
 $(document).ready(main); 
